b7e8c127926b876ecceafecd7ff871069a256093
Eveline Guimarães <59440207+evelinemg@users.noreply.github.com>
2022-02-10 18:21:05 -0300

    Update forca.js

--- forca.js
+++ forca.js
@@ -2,6 +2,7 @@ var p=0;
 var count=0;
 var word=''
 var letraUser=''
+var userPerdeu=false
 var botaoIniciar = document.querySelector("#iniciar-jogo");
 botaoIniciar.addEventListener('click',function(event){
   word=buscaPalavra();
@@ -11,8 +12,14 @@ botaoIniciar.addEventListener('click',function(event){
   addNumeroLinhas(espacos);
   var tecla=document.getElementById('keyboard');
   tecla.onclick=teclaEnter;
+  document.addEventListener('keydown', (event) => {
+  var name = event.key;
+  letraUser=name.toUpperCase();
+  if(verifica(letraUser)){
+    verificaLetra(letraUser);
+  }
 
-
+}, false);
 });
 
 function split(word){
@@ -23,7 +30,11 @@ function split(word){
 
 function teclaEnter(event) {
   letraUser=event.target.id;
-  verificaLetra(letraUser);
+  if(verifica(letraUser)){
+    verificaLetra(letraUser);
+  };
+
+
 }
 
 function verificaLetra(letra){
@@ -31,120 +42,67 @@ function verificaLetra(letra){
   n=0;
   for(var i=0; i<espacos;i++){
     if(letra==letras[i]){
-      console.log(letra, letras[i])
       index[n]=i;
       n=n+1;
       count=count+1;
     }
   }
-  addLetrasPosicao(letra, index);
-  addLetraCerta(letra);
-
-
   if(n==0){
-  desenhaMan(p);
+    desenhaMan(p);
     addLetraErrada(letra);
     p=p+1;
   }
-  if(count==letras.length){
-    venceu();
+  else{
+    addLetrasPosicao(letra, index);
+    addLetraCerta(letra);
   }
   if(p==6){
     perdeu(word);
-  }
+    userPerdeu=true;
 
+  }
+  if(count==letras.length){
+    venceu();
+    userPerdeu=true;
+  }
 };
-/*
-function desenhaMan(p){
-  if(p==0){
-    desenhaCabeca();
-  }
-  else if(p==1){
-    desenhaCorpo();
-  }
-  else if(p==2){
-    desenhaBracoDireito();
 
-  }
-  else if(p==3){
-    desenhaBracoEsquerdo();
-
-  }
-  else if(p==4){
-    desenhaPernaDireita();
-  }
-  else if(p==5){
-    desenhaPernaEsquerda();
-   mensagemPerdeu(word);
-  }
-}
-
-function verificaLetraPalavra(letra,word){
-  if(letra.length==1){
-    verificaLetra(letra);
-  }
-  else{
-       if(word===letra){
-         messagemVenceu();
-         mostraPalavra(word);
-       }
-       else{
-         alert("Não é a palavra correta. Tente mais uma vez");
-       }
-
-  }*/
-
-/*botaoIniciar.addEventListener("click", function(event){
-  pincel.clearRect(0, 0, 800, 600);
-  desenhaForca();
-  word = buscaPalavra();
-  letras=split(word);
-  espacos=letras.length;
-  console.log(word);
-  quantidadeRetangulos(50,550, espacos);
-  p=0;
-});
-
-
-var letra= '';
-var botaoNovaPalavra = document.querySelector("#nova-palavra");
-botaoNovaPalavra.addEventListener("click", function(event){
-  event.preventDefault();
-  var areaLetra=document.querySelector("#input-nova-palavra");
-  letra=areaLetra.value;
-  letra=letra.toUpperCase();
-  verificaLetraPalavra(letra,word);
-  var form = document.querySelector("input-nova-palavra");
-  areaLetra.value='';
-
-});
-
-
- function mostraPalavra(word){
-   console.log(word);
-   word=word.split('');
-   for(var i=0;i<word.length;i++){
-     var x=60+(i*50);
-     addLetra(word[i],x,590)
-
-   }
- }*/
-/*myMain();
- function myMain() {
-   document.getElementById("keyboard").onclick = buton;
- }
- function buton(e) {
-  console.log(e.target.id);
-}*/
 function addLetraErrada(letra){
-  console.log('#'+letra);
   a=document.querySelector('#'+letra);
   a.setAttribute('data-state', 'absent');
 
 }
 function addLetraCerta(letra){
-  console.log('#'+letra);
   a=document.querySelector('#'+letra);
   a.setAttribute('data-state', 'valid');
+
+}
+function validaLetra(letra){
+  var letters = /^[A-Za-z]+$/;
+   if(letra.match(letters)){
+      return true;
+     }
+   else{
+     return false;
+     }
+}
+function repeticao(letra){
+  a=document.querySelector('#'+letra);
+  b=a.getAttribute('data-state');
+  if(b=='valid' || b=='absent'){
+    return false;
+  }
+  else{
+    return true;
+  }
+}
+function verifica(letra){
+  if(validaLetra(letraUser)&& repeticao(letraUser)&&!userPerdeu){
+    return true;
+  }
+  else{
+    return false;
+  }
+
 
 }
